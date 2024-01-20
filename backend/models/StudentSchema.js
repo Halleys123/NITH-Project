3addf868e6dccacc2959840b149b0622f81e29a8
Himanshu Gupta <himanshu12102004@gmail.com>
2024-01-20 20:03:26 +0530

    filtering logic corrected

--- backend/models/StudentSchema.js
+++ backend/models/StudentSchema.js
@@ -10,12 +10,16 @@ const studentSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
+  lateTimes: { type: Number, default: 0 },
+  isAllowed: { type: Boolean, default: true },
   history: [
     {
       exitDate: { type: Date },
       entryDate: { type: Date },
       exitGate: { type: Number },
       entryGate: { type: Number },
+      isLate: { type: Boolean, default: false },
+      expectedEntryDate: { type: Date },
       reason: { type: String, enum: ["market", "home"], default: "market" },
     },
   ],
